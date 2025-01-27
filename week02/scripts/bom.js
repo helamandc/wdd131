ca29aa6933b406d1df54fe0a4916f0fe5c457118
helamandc <manlehcastillo@gmail.com>
2025-01-27 11:51:16 +1100

    week 2 - bom add chapters js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,18 +1,32 @@
 const input = document.getElementById('favchap');
-
 const button = document.querySelector('button');
-
 const list = document.getElementById('list');
 
-const li = document.createElement('li');
 
-const delButton = document.createElement('button');
+button.addEventListener('click', function () {
+    const li = document.createElement('li');
+    const delButton = document.createElement('button');
+    if (input.value.trim() !== '') {
+        li.textContent = input.value;
+        delButton.textContent = '❌';
+        li.appendChild(delButton);
+        list.appendChild(li);
+
+        input.value = '';
+        input.focus();
+
+        delButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+
+    } else {
+        input.focus();
+    }
+})
 
 
-li.textContent = input.value;
 
-delButton.textContent = '❌';
 
-li.append(delButton);
 
-list.append(li);
+
