ff07763cff2bd9b28c222b080f19983c8852b28d
helamandc <manlehcastillo@gmail.com>
2025-01-27 14:50:48 +1100

    debugging practice

---
+++ week02/scripts/debugging.js
@@ -0,0 +1,17 @@
+// const PI = 3.14;
+// const radius = 3;
+// let area = 0;
+// area = radius * radius * PI;
+// console.log(area);
+// const radius1 = 4;
+// area = radius1 * radius * PI;
+// console.log(area);
+
+const PI = 3.14;
+let area = 0;
+function circleArea(radius) {
+    const area = radius * PI;
+    return area;
+}
+area = circleArea(3);
+console.log(area);
